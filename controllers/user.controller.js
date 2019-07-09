bd6d2347d0dc33e0ce36eb4d55a6619560c5386c
Billy Vlachos <williamcaminiti@live.com>
2019-07-09 13:30:27 +0200

    Implement the login or register function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,129 +2,55 @@
 const jwt = require('./../helpers/jwt');
 const User = require('./../models/user.model');
 
+// Generates a new token
+const generateToken = (userId, username) => {
+    return jwt.toJwt({
+        userId: userId,
+        username: username
+    });
+}
 
 /**
  * Action that checks if a username already exists or
  * creates a new one and returns a token with userID and username
  */
 const loginOrRegister = (req, res, next) => {
-    throw new Error("Not Implemented Exception");
-
-    // Check if User exists
-    // If he exists, generate token with user id and username and return it
-    // If not, create the user
-    // Retrieve the newly created user
-    // Generate token with user id and username and return it
-}
-
-// SAMPLE CODE BELOW
-// TO REMOVE
-const login = (req, res, next) => {
-    // Retrieve the email and password
-    const { email, password } = req.body;
-    // Check if email and password exist
-    if (email && password) {
-        // Find the usr based on email address
-        User
-            .findOne({
-                where: { email: email }
-            })
-            .then(user => {
-                // Check if user was not found
-                if (!user) {
-                    return res.status(400).send({
-                        message: "A user with this e-mail address does not exist."
-                    });
-                }
-
-                // Hash provided password and compare
-                const pwCorrect = bcrypt.compareSync(password, user.password);
-                // Check if comparison returned true
-                if (pwCorrect) {
-                    // Password was correct
-                    // Return a JWT token with the user id
-                    const token = jwt.toJwt({
-                        userId: user.id
-                    });
-                    // Send back to client
-                    return res.send({
-                        jwt: token
-                    })
-                } else {
-                    // Password was not correct
-                    // Return 400
-                    return res.status(400).send({
-                        message: "The password was incorrect."
-                    })
-                }
-            })
-            .catch(err => {
-                return res.status(500).send({
-                    message: "Something went wrong. Please try again later!",
-                    error: err
-                })
-            })
-
-
-    } else {
-        // Return invalid
-        return res.status(400).send({
-            message: "Please supply a valid email and password"
-        })
-    }
-}
-
-// TO REMOVE
-const signup = (req, res, next) => {
-    // Retrieve the email and password and confirmation
-    const { email, password, password_confirmation } = req.body;
-
-    // Check if passwords match
-    if (password !== password_confirmation) return res.status(400).send({
-        message: "Password and confirmation passwords do not match!"
+    // Check if the user passed a username
+    const { username } = req.body;
+    if (!username) return res.status(404).send({
+        message: "Please specify a valid username to proceed!"
     });
 
-    // Check if e-mail is valid
-    if (!validateEmail(email)) return res.status(400).send({
-        message: "E-mail address is not valid!"
-    });
-
-    // Create the user account
-    const account = {
-        email: email,
-        password: bcrypt.hashSync(password, salt)
-    }
-
-    // Check if user already exists
-    // Not sure if it's alright to return a message
-    // That informs user that this e-mail is already taken
-    User.count({ where: { email: email } })
-        .then(count => {
-            // Check if at least one account exists
-            // with the specified email
-            if (count > 0) return res.status(422).send({
-                message: "Cannot create account!"
-            });
-
-            // If code reached this far
-            // Perform registration
-            User.create(account)
-                .then(user => {
-                    // Remove password from user
-                    const { email, id } = user;
-                    // Return success with new user object
-                    return res.status(201).json({ email, id });
-                })
-                .catch(err => {
-                    // Cannot create user
-                    return next(err);
-                })
+    // Check if user exists in the database
+    return User.findOne({ where: { username: username } })
+        .then(user => {
+            // Check if user was not found
+            if (!user) {
+                // User not found, create a new one
+                User.create({ username: username })
+                    .then(u => {
+                        // Set status 201 :: user created
+                        return res.status(201).send({
+                            // Generate the token and return it
+                            jwt: generateToken(u.id, username)
+                        });
+                    })
+                    .catch(err => {
+                        // User not created, return error
+                        return res.status(500).send({
+                            message: "Cannot create user. Please try again later"
+                        })
+                    });
+            } else {
+                // Valid user exists, generate token and return it
+                // Return a JWT token with the user id and username
+                return res.status(200).send({
+                    // Generate the token and return it
+                    jwt: generateToken(u.id, username)
+                });
+            }
         })
-        .catch(err => {
-            // Cannot create user
-            return next(err);
-        })
-
+        .catch(e => next(e));
 }
 
 // Export auth controller functions
