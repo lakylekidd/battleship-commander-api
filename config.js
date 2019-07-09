edd797a3828b01172c7bed2f2897b46881e68cb5
Billy Vlachos <williamcaminiti@live.com>
2019-07-09 10:14:07 +0200

    Add database URL

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Export the configuration module
 module.exports = {
     name: 'battleship-commader-api',
+    connectionString: process.env.DATABASE_UTL || 'postgres://postgres:secret@localhost:5432/postgres',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'localhost',
     environment: process.env.NODE_ENV,
