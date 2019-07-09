af85daf4ee1d438aca08de877b41e2d1dd57773a
Billy Vlachos <williamcaminiti@live.com>
2019-07-09 11:44:30 +0200

    Update the documentation of the game controller

--- controllers/game.controller.js
+++ controllers/game.controller.js
@@ -2,24 +2,38 @@
 const jwt = require('./../helpers/jwt');
 const User = require('./../models/user.model');
 
-
-
+/**
+ * Action that returns a list of all available
+ * games (gameState === 0)
+ */
 const getAvailableGames = (req, res, next) => {
     throw new Error("Not Implemented Exception");
 }
-
+/**
+ * Action that creates a new game session
+ * and sets requesting user as the owner
+ */
 const createNewGameSession = () => {
     throw new Error("Not Implemented Exception");
 }
-
+/**
+ * Action that performs a fire event of a user
+ * towards his opponent.
+ */
 const fire = () => {
     throw new Error("Not Implemented Exception");
 }
-
+/**
+ * Action that allows subscriptions for the game stream
+ * just like the chat app sends all new messages to everybody
+ */
 const gameStream = () => {
     throw new Error("Not Implemented Exception");
 }
-
+/**
+ * Action that allows a player to join a new game
+ * Only allow user to join if game is still active
+ */
 const join = () => {
     throw new Error("Not Implemented Exception");
 }
